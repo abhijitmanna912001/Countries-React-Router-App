caa5c400460fcaeb31f9b00315196fc7f26c75e2
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2025-07-10 13:47:27 +0530

    Displaying Countries List Completed

--- app/routes/countries.tsx
+++ app/routes/countries.tsx
@@ -1,3 +1,31 @@
-export default function Countries() {
-  return <div>Countries Page</div>;
+import { Link } from "react-router";
+import type { Route } from "./+types/countries";
+
+export async function clientLoader() {
+  const res = await fetch(
+    "https://restcountries.com/v3.1/independent?status=true"
+  );
+  const data = await res.json();
+  return data;
+}
+
+export default function Countries({
+  loaderData,
+}: Readonly<Route.ComponentProps>) {
+  return (
+    <div>
+      <ul>
+        {loaderData.map((country: any, key: number) => (
+          <li key={key}>
+            <Link to={`/countries/${country.name.common}`}>
+              {country.name.common}
+            </Link>
+            <div>
+              Region: {country.region} | Population {country.population}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
